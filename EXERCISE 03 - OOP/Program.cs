9a1cd5b5d03d7033be1297b17eea4d58b11b0e1d
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-26 10:26:46 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 03 - OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace EXERCISE_03___OOP{
+    class Program{
+        //-----> INICIO
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Student Stdt = new Student();
+
+            Console.Write("Digite o Nome do aluno: "); Stdt.Name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Digite as 3 notas do aluno: ");
+            Stdt.Grade1 = double.Parse(Console.ReadLine());
+            Stdt.Grade2 = double.Parse(Console.ReadLine());
+            Stdt.Grade3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("NOTA FINAL= " + Stdt.FinalGrade().ToString("F2"));
+            if (Stdt.Approved()){
+                Console.WriteLine("APROVADO");
+            }
+
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //-----> FIM
+    }
+}
---
+++ EXERCISE 03 - OOP/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_03___OOP{
+    //----------------------START | OBJ STUDENT------------------------//
+    class Student{
+        public string Name;
+        public double Grade1, Grade2, Grade3;
+        //------------------------ MET. CLASS -------------------------------//
+        public double FinalGrade(){
+            return Grade1 + Grade2 + Grade3;
+        }
+        public bool Approved() { 
+            if (FinalGrade() >= 60){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        public double RemainingGrade(){
+            if (Approved()){
+                return 0;
+            }
+            else {
+                return 60 - FinalGrade();
+            }
+        }
+    }
+    //--------------------------- END --------------------------------// 
+}
